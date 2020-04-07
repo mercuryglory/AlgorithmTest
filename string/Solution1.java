b844c962db16b21299fd678042905e7a4eaaf442
mercuryglory <wzhseu@163.com>
2020-04-07 23:43:20 +0800

    string test1

---
+++ string/Solution1.java
@@ -0,0 +1,30 @@
+package string;
+
+/**
+ * created by mercury on 2020-04-07
+ * 实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy
+ */
+public class Solution1 {
+
+    public static String replaceSpace(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replaceSpace("We Are Happy"));
+
+    }
+}
