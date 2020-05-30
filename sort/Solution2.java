b23a7aa8c084ab67c0d6ec254f5788baeaf0ed39
mercuryglory <wzhseu@163.com>
2020-05-30 18:47:16 +0800

    heap sort

--- sort/Solution2.java
+++ sort/Solution2.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class Solution2 {
 
-    public static ArrayList<Integer> getLeastNumbers(int[] input,int k) {
+    public static ArrayList<Integer> getLeastNumbers2(int[] input, int k) {
         ArrayList<Integer> list = new ArrayList<>();
         if (input == null || input.length < k) {
             return list;
@@ -22,10 +22,76 @@ public class Solution2 {
         return list;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {4, 5, 1, 6, 2, 7, 3, 8};
-        System.out.println(getLeastNumbers(arr, 4));
+    public static ArrayList<Integer> getLeastNumbers(int[] input, int k) {
 
+        ArrayList<Integer> list = new ArrayList<>();
+        if (input == null || input.length < k) {
+            return list;
+        }
+        heapSort(input, k);
+        for (int i = 0; i < k; i++) {
+            list.add(input[i]);
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 1, 6, 2, 7, 2, 8};
+        System.out.println(getLeastNumbers(arr, 2));
+
+    }
+
+
+    /**
+     * 快排的时间复杂度为nlogn，堆排序则是nlogk
+     * 对{@link SortTest#heapSort(int[])}做一个调整
+     *
+     * 整体思路是开始构建大顶堆，只需要构建前k个数的堆，得到前k个数的最大值在堆顶
+     * 然后从第k个元素开始，与当前堆的堆顶做比较，小于就交换并调整，注意这里调整时仍然只需要调整前k个数，因为只要保证了
+     * 前k个数的最大值在堆顶，那么k之后的只要有比其小的就一定会将该堆顶数调整到后面，最后堆里面的前k个数就是结果
+     *
+     * 注意这里的堆中不保证排序效果，因为只是部分构建堆，并不是堆顶与末尾元素交换
+     *
+     */
+    private static void heapSort(int[] arr, int k) {
+        for (int i = k / 2 - 1; i >= 0; i--) {
+            adjustMaxHeap(arr, i, k);
+        }
+
+        //从第k个元素开始分别与最大堆的最大值作比较，如果比最大值小，则替换并调整堆
+        for (int i = k; i < arr.length; i++) {
+            if (arr[i] < arr[0]) {
+                swap(arr, 0, i);
+                adjustMaxHeap(arr, 0, k);
+            }
+        }
+    }
+
+
+    public static void adjustMaxHeap(int[] arr, int i, int len) {
+        int temp = arr[i];
+        for (int j = i * 2 + 1; j < len; j = j * 2 + 1) {
+            if (j < len - 1 && arr[j] < arr[j + 1]) {
+                j++;
+            }
+            if (arr[j] > temp) {
+                arr[i] = arr[j];
+                i = j;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;
+
+    }
+
+
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
 
--- sort/SortTest.java
+++ sort/SortTest.java
@@ -5,7 +5,7 @@ package sort;
  */
 public class SortTest {
 
-    private static final int COUNT = 10000000;
+    private static final int COUNT = 100000;
 
     public static void main(String[] args) {
         int[] arr = new int[COUNT];
@@ -14,8 +14,8 @@ public class SortTest {
             arr[i] = random;
         }
 
-        quickSort(arr);
-//        heapSort(arr);
+//        quickSort(arr);
+        heapSort(arr);
         printArr(arr);
 
     }
@@ -73,35 +73,48 @@ public class SortTest {
      * 3、堆的尺寸缩小1，并把新的数组顶端数据调整到相应位置
      * 4、重复步骤，直到堆的尺寸为1
      *
+     * 整体主要由构建初始堆+交换堆顶元素和末尾元素并重建堆两部分组成
+     *
      */
-    private static void heapSort(int[] arr) {
-        int len = arr.length - 1;
-        for (int i = (len - 1) / 2; i >= 0; i--) {
-            adjustMaxHeap(arr, i, len);
+    public static void heapSort(int[] arr) {
+        //1 构建大顶堆
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            //从第一个非叶子节点从下至上，从右至左调整结构
+            adjustMaxHeap(arr, i, arr.length);
         }
 
-        for (int i = len; i > 0; i--) {
-            swap(arr, 0, i);
-            adjustMaxHeap(arr, 0, i - 1);
+        //2 交换堆顶元素与末尾元素+调整堆结构，交换了之后调整时把当面的末尾（j)排除掉就可以了，因为j处已经是最大
+        for (int j = arr.length - 1; j > 0; j--) {
+            swap(arr, 0, j);
+            adjustMaxHeap(arr, 0, j);
         }
+
+
     }
 
-
-    private static void adjustMaxHeap(int[] arr, int index, int len) {
-        int left = index * 2 + 1;
-        int right = left + 1;
-        //记录最大值下标
-        int maxIndex = left;
-        if (left > len) {
-            return;
+    /**
+     * 调整大顶堆
+     */
+    private static void adjustMaxHeap(int[] arr, int i, int len) {
+        int temp = arr[i];
+        //从i的左子节点开始
+        for (int j = i * 2 + 1; j < len; j = j * 2 + 1) {
+            //如果左子节点小于右子节点，K指向右子节点
+            if (j < len - 1 && arr[j] < arr[j + 1]) {
+                j++;
+            }
+            //如果子节点大于父节点，将子节点赋值给父节点
+            if (arr[j] > temp) {
+                //这一步是因为如果父节点被调整到下面了，下面可能还有比父节点大的值，继续调整
+                arr[i] = arr[j];
+                i = j;
+            } else {
+                //左右子节点都比父节点小，结束
+                break;
+            }
         }
-        if (right <= len && arr[left] < arr[right]) {
-            maxIndex = right;
-        }
-        if (arr[maxIndex] > arr[index]) {
-            swap(arr, index, maxIndex);
-            adjustMaxHeap(arr, maxIndex, len);
-        }
+        //父节点最后落位的值
+        arr[i] = temp;
     }
 
     private static void swap(int[] arr, int a, int b) {
