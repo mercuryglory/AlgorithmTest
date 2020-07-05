27f9fed073971aae2c6c1aaccd66895bb1fc1a36
mercuryglory <wzhseu@163.com>
2020-07-05 13:07:04 +0800

    min number in rotate array

--- sort/Solution.java
+++ sort/Solution.java
@@ -10,21 +10,27 @@ package sort;
 public class Solution {
 
     public static void main(String[] args) {
-        int[] arr = {3, 4, 5, 0, 1, 2};
+        int[] arr = {3, 4, 5, 6, -1, 1, 2};
+//        int[] arr = {1, 0, 1, 1, 1};
         System.out.println(minNumberInRotateArray(arr));
     }
 
     /**
+     * 旋转后的数组可以划分为两个有序子数组，前面子数组的大小都大于后面子数组的元素，最小的元素实际就是两个子数组的分界线
+     *
      * 用二分法解答
      * mid=low+(high-low)/2  有三种情况
      * 1、array[mid] > array[high]
      * 类似[3,4,5,6,0,1,2] 最小数字一定在mid右边  low=mid+1
      * 2、array[mid] == array[high]
-     * 类似[1,0,1,1,1]，最小数字不好判断在哪一边，一个个试  high=high-1
+     * 类似[1,0,1,1,1]、[1,1,1,0,1] 最小数字不好判断在哪一边，一个个试  high=high-1
      * 3、array[mid] < array[high]
      * 类似[2,2,3,4,5,6,6]。最小数字一定是mid或左边     high=mid
      */
     public static int minNumberInRotateArray(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
         int low = 0;
         int high = array.length - 1;
         while (low < high) {
