b43d68ccb172898d48538c15da488c9a2cf74521
mercuryglory <wzhseu@163.com>
2020-07-08 23:03:09 +0800

    update

--- btree/Solution1.java
+++ btree/Solution1.java
@@ -13,8 +13,8 @@ public class Solution1 {
         root1.left.right = new TreeNode(4);
 
         TreeNode root2 = new TreeNode(5);
-        root2.left = new TreeNode(6);
-        root2.right = new TreeNode(9);
+//        root2.left = new TreeNode(6);
+//        root2.right = new TreeNode(7);
 
         root1.right = new TreeNode(5);
         root1.right.left = new TreeNode(6);
@@ -33,7 +33,7 @@ public class Solution1 {
                 result = doesTree1HaveTree2(root1, root2);
             }
 
-            //找不到，再去root的左儿子当作起点，去判断是否包含tree2
+            //找不到，再从tree1的左儿子当作起点，去判断是否包含tree2
             if (!result) {
                 result = hasSubTree(root1.left, root2);
             }
